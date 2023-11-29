b9c1bef05a7b9a0f74377ec8e5de4e3f9b5e363e
BasKrueger <147401575+BasKrueger@users.noreply.github.com>
2023-11-29 11:43:03 +0100

    Add Turret

---
+++ Assets/Script/Tower.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    List<Enemy> enemies = new List<Enemy>();
+
+    public Transform bulletSpawnPoint;
+    public Bullet projectile;
+    public float fireCooldown_;
+    private float fireCooldown = 1;
+
+    private void Update()
+    {
+        while (enemies.Contains(null))
+        {
+            enemies.Remove(null);
+        }
+
+        if (fireCooldown < 0)
+        {
+            Fire();
+            fireCooldown = fireCooldown_;
+        }
+
+        fireCooldown -= Time.deltaTime;
+    }
+
+    void Fire()
+    {
+        var closest = ClosestEnemy();
+        if (closest == null) return;
+
+        var bullet = Instantiate(projectile);
+        bullet.transform.position = bulletSpawnPoint.position;
+        bullet.Fire(closest.transform.position - transform.position);
+    }
+
+    Enemy ClosestEnemy()
+    {
+        Enemy closest = null;
+        foreach(Enemy enemy in enemies)
+        {
+            if (closest == null)
+            {
+                closest = enemy; 
+                continue;
+            }
+            
+            if(Vector3.Distance(enemy.transform.position, transform.position) < Vector3.Distance(closest.transform.position, transform.position))
+            {
+                closest = enemy;
+            }
+        }
+
+        return closest;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Enemy>() != null)
+        {
+            enemies.Add(other.GetComponent<Enemy>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Enemy>() != null)
+        {
+            enemies.Remove(other.GetComponent<Enemy>());
+        }
+    }
+}
