d7dcfea2c560c44506f4c4388fc579cd66511cee
BasKrueger <147401575+BasKrueger@users.noreply.github.com>
2023-12-04 16:33:40 +0100

    updated bullet

---
+++ Assets/Rest/Script/Bullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    Vector3 direction;
+
+    public float penetration = 0;
+
+    public void Fire(Vector3 direction, bool lockY = false, float bonusSpeed = 0)
+    {
+        speed += bonusSpeed;
+        direction = direction.normalized;
+        if (lockY)
+        {
+            direction.y = 0;
+        }
+        this.direction = direction;
+        Destroy(this.gameObject, 10);
+    }
+
+    private void Update()
+    {
+        GetComponent<Rigidbody>().velocity = direction * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Enemy>() != null ) 
+        {
+            other.GetComponent<Enemy>().TakeDamage(1);
+
+            penetration--;
+            if(penetration < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
